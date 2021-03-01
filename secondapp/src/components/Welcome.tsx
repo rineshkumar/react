405de4519c9d62fd63adeed68bafee870e731ad0
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-02 10:27:35 +1100

    Changes for react router

--- secondapp/src/components/Welcome.tsx
+++ secondapp/src/components/Welcome.tsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 interface WelcomeProps{
     name : string
 }
 const Welcome: React.FC<WelcomeProps> = ({name}): JSX.Element => {
     const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+    const [message, setMessage]= useState<string>('');
+
     setInterval(() => {
         setTime(new Date(Date.now()));
       }, 1000);
+
+      const updateMessage = (e:ChangeEvent<HTMLInputElement>):void => {
+          const {value} = e.target;
+            setMessage(value);
+      }
     return (
         <div>
             Welcome {name} !! {time.toUTCString()}
+            <input type="text"
+                value={message}
+                onChange={updateMessage}
+            >
+            </input>
+            Your Message : {message}
         </div>
     )
 }
