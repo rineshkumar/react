000c918cfe067f7157cdbcd89a2ff035ba2932f3
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-02 10:23:00 +1100

    updating a state

--- secondapp/src/App.tsx
+++ secondapp/src/App.tsx
@@ -16,9 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Welcome name="Rinesh"/>
           Learn React
         </a>
+        <Welcome name="Rinesh"/>
+        
       </header>
     </div>
   );
