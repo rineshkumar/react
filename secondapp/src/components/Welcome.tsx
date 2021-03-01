2afe86909bcfd0d6c2edbf7ae8c2d7eba843feb2
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-02 10:04:14 +1100

    Using the useState hook

--- secondapp/src/components/Welcome.tsx
+++ secondapp/src/components/Welcome.tsx
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
 interface WelcomeProps{
     name : string
 }
 const Welcome: React.FC<WelcomeProps> = ({name}): JSX.Element => {
+    const [time, setTime] = useState<Date>(() => new Date(Date.now()));
     return (
         <div>
-            Welcome {name}!!
+            Welcome {name} !! {time.toUTCString()}
         </div>
     )
 }
