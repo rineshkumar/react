53ea93c065af13c816044c93a5dfd02359bca877
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 16:03:11 +1100

    Adding thunk middle ware to store

--- thirdapp/react-redux-example/src/js/store/store.js
+++ thirdapp/react-redux-example/src/js/store/store.js
@@ -1,6 +1,13 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import { rootReducer } from "../reducer/reducer"
 import { forbiddenWordsMiddleware } from "../middleware/middleware"
-const store = createStore(rootReducer, applyMiddleware(forbiddenWordsMiddleware));
+import thunk from "redux-thunk"
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware,thunk))
+);
 //https://medium.com/@etherealm/named-export-vs-default-export-in-es6-affb483a0910
 export default store
