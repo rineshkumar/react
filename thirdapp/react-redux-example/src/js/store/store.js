39dff9604ec7d25267962578072a56c929d3a534
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 12:53:52 +1100

    File formatted

--- thirdapp/react-redux-example/src/js/store/store.js
+++ thirdapp/react-redux-example/src/js/store/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux"
-import {rootReducer} from "../reducer/reducer   "
+import {rootReducer} from "../reducer/reducer"
 
 const store = createStore(rootReducer);
 //https://medium.com/@etherealm/named-export-vs-default-export-in-es6-affb483a0910
