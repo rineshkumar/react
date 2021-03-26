6776d33d657f93015edd69bacf89690c046f59df
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 15:46:12 +1100

    Formatted

--- thirdapp/react-redux-example/src/js/store/store.js
+++ thirdapp/react-redux-example/src/js/store/store.js
@@ -1,6 +1,6 @@
-import {createStore,applyMiddleware} from "redux"
-import {rootReducer} from "../reducer/reducer"
-import {forbiddenWordsMiddleware} from "../middleware/middleware"
-const store = createStore(rootReducer,applyMiddleware(forbiddenWordsMiddleware));
+import { createStore, applyMiddleware } from "redux"
+import { rootReducer } from "../reducer/reducer"
+import { forbiddenWordsMiddleware } from "../middleware/middleware"
+const store = createStore(rootReducer, applyMiddleware(forbiddenWordsMiddleware));
 //https://medium.com/@etherealm/named-export-vs-default-export-in-es6-affb483a0910
 export default store
