3b14a9c0ab021f6ce978b27861f00b9da5e8e1ba
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 12:33:36 +1100

    updated reducer

--- thirdapp/react-redux-example/src/js/reducer/reducer.js
+++ thirdapp/react-redux-example/src/js/reducer/reducer.js
@@ -1,11 +1,18 @@
-import {initialState} from "../state/state"
-import {ADD_ARTICLE} from "../actions/actiontypes"
-import {addArticle} from "../actions/actions"
+import { initialState } from "../state/state"
+import { ADD_ARTICLE } from "../actions/actiontypes"
+import { addArticle } from "../actions/actions"
 
 const rootReducer = (state = initialState, action) => {
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
-    if(action.type === ADD_ARTICLE){
-        state.articles.push(action.payload)
+    if (action.type === ADD_ARTICLE) {
+        //using push ==> state is mutable ==> Wrong 
+        //state.articles.push(action.payload)
+        //use object.assign and concat
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+        return Object.assign({}, state,
+            {
+                articles: state.articles.concat(action.payload)
+            });
     }
-    return state ;
+    return state;
 }
