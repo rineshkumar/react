e6ea0e397e6a6dd8c3b023e5349993a15e308e25
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 12:54:14 +1100

    Added browser test so that store is available in browser console.

--- thirdapp/react-redux-example/src/index.js
+++ thirdapp/react-redux-example/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import browsertest from "./js/browsertest"
 ReactDOM.render(
   <React.StrictMode>
     <App />
