5186d5c70b9c4e8a64dad92a00e9082ed74a5908
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 16:12:01 +1100

    Added an action producer using thunk

--- thirdapp/react-redux-example/src/js/actions/actions.js
+++ thirdapp/react-redux-example/src/js/actions/actions.js
@@ -3,4 +3,14 @@ const addArticle = (payload) => {
     return { type: ADD_ARTICLE, payload }
 }
 
-export { addArticle }
+const getData = () => {
+    return (dispatch) => {
+        return fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json)
+        .then(json => {
+            dispatch({type : "DATA_LOADED", payload: json})
+        })
+    }
+}
+
+export { addArticle , getData}
