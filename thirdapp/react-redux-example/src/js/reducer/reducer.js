1221b939d26c81f25e9c0fb86d823e0b54fd7ee6
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 12:40:20 +1100

    Added a store using using reducer

--- thirdapp/react-redux-example/src/js/reducer/reducer.js
+++ thirdapp/react-redux-example/src/js/reducer/reducer.js
@@ -16,3 +16,5 @@ const rootReducer = (state = initialState, action) => {
     }
     return state;
 }
+
+export {rootReducer}
---
+++ thirdapp/react-redux-example/src/js/store/store.js
@@ -0,0 +1,6 @@
+import {createStore} from "redux"
+import {rootReducer} from "../reducer/reducer   "
+
+const store = createStore(rootReducer);
+//https://medium.com/@etherealm/named-export-vs-default-export-in-es6-affb483a0910
+export default store
