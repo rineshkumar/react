40bbdf891eda7ae2b79e170df77c0e5c4b7fb320
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-26 16:13:30 +1100

    updated code

--- thirdapp/react-redux-example/src/js/actions/actions.js
+++ thirdapp/react-redux-example/src/js/actions/actions.js
@@ -1,4 +1,4 @@
-import { ADD_ARTICLE } from "./actiontypes"
+import { ADD_ARTICLE,DATA_LOADED } from "./actiontypes"
 const addArticle = (payload) => {
     return { type: ADD_ARTICLE, payload }
 }
@@ -8,7 +8,7 @@ const getData = () => {
         return fetch("https://jsonplaceholder.typicode.com/posts")
         .then(response => response.json)
         .then(json => {
-            dispatch({type : "DATA_LOADED", payload: json})
+            dispatch({type : DATA_LOADED, payload: json})
         })
     }
 }
