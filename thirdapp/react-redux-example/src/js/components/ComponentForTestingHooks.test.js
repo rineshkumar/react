9fe294eff85db366b8171b00437b5f7fd0cb3b42
Rinesh Kumar <rineshkumar@gmail.com>
2021-03-29 15:56:32 +1100

    snapshots, mount , render

---
+++ thirdapp/react-redux-example/src/js/components/ComponentForTestingHooks.test.js
@@ -0,0 +1,23 @@
+import { cleanup, fireEvent, render } from "@testing-library/react"
+import ComponentForTestingHooks from "./ComponentForTestingHooks"
+import ParentComponent from "./ParentComponent";
+
+
+afterEach(cleanup)
+
+it("Testing hooks ", () => {
+    const { getByText } = render(<ComponentForTestingHooks />);
+    expect(getByText(/Initial/i).textContent).toBe("Initial State")
+    fireEvent.click(getByText("State Change Button"))
+    expect(getByText(/Initial/i).t7extContent).toBe("Initial State Changed")
+})
+
+it("Testing props ", () => {
+    const { getByText } = render(
+        <ParentComponent>
+            <ComponentForTestingHooks />
+        </ParentComponent>);
+    expect(getByText(/Rinesh/i).textContent).toBe("Rinesh")
+    fireEvent.click(getByText("Change Name"));
+    expect(getByText(/Steve/i).textContent).toBe("Steve")
+})
